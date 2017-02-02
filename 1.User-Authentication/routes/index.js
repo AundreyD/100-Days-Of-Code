438cea84033cd0b9063677a639a76e3724038dd0
Aundrey Drummond <aundreydrummond@aundreys-mbp.attlocal.net>
2017-02-02 01:48:19 -0500

    Revert "Update"
    This reverts commit 9010f095261b9c9df5d73b51280b50919de6bc2d.

--- 1.User-Authentication/routes/index.js
+++ 1.User-Authentication/routes/index.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
-var mid = require('../middleware');
 
 
 
-//Get /profile
-router.get('/profile',mid.requiresLogin, function(req, res, next){
-  // if(! req.session.userId){
-  //   var err= new Error('You are not authorized to view this page');
-  //   err.status = 403;
-  //   return next(err);
-  // }
+//Get .profile
+router.get('/profile', function(req, res, next){
+  if(! req.session.userId){
+    var err= new Error('You are not authorized to view this page');
+    err.status = 403;
+    return next(err);
+  }
   User.findById(req.session.userId)
     .exec((error, user) => {
       if(error){
@@ -36,7 +35,7 @@ router.get('/logout', (req, res, next) => {
   }
 });
 //GET /logins
-router.get('/login',mid.loggedOut, (req, res, next) => {
+router.get('/login', (req, res, next) => {
   return res.render('login', {title: 'Log in'});
 });
 
@@ -61,7 +60,7 @@ router.post('/login', (req, res, next) => {
 });
 
 // GET /register
-router.get('/register', mid.loggedOut, function(req, res, next) {
+router.get('/register', function(req, res, next) {
   return res.render('register', { title: 'Sign Up' });
 });
 
